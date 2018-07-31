6a39ef8635c5f7c52aff8cc2bdfa139854d5f19c
mmmaaaggg <mmmaaaggg@163.com>
2018-07-31 09:11:53 +0800

    add run script

---
+++ run.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2018/7/30 18:15
+@File    : run.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+
+from direstplus import start_service
+
+
+if __name__ == '__main__':
+    start_service()
