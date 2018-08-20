dcde5cfe38f38316c5dabeace915dd117d1f84f6
mmmaaaggg <mmmaaaggg@163.com>
2018-08-20 14:38:24 +0800

    (-1010, -1020) 错误，自动重新登录

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding='utf-8') as rm:
     long_description = rm.read()
 
 setup(name='DIRestPlus',
-      version='0.1.6',
+      version='0.1.7',
       description='基于Restplus实现同花顺iFinD接口分布式调用',
       long_description=long_description,
       long_description_content_type='text/markdown',
